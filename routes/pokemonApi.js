643f74a3ac4ea49cc2f630c1cae2ae7da213532f
unknown <alviss1004@gmail.com>
2022-10-24 14:37:56 +0700

    added delete/update api

--- routes/pokemonApi.js
+++ routes/pokemonApi.js
@@ -23,7 +23,7 @@ const pokemonTypes = [
   "rock",
   "water",
 ];
-
+//Get all pokemons
 router.get("/", (req, res, next) => {
   const allowedFilter = ["type", "search", "page", "limit"];
   try {
@@ -90,7 +90,7 @@ router.get("/", (req, res, next) => {
     next(error);
   }
 });
-
+//Get pokemon details by id
 router.get("/:id", (req, res, next) => {
   let { params } = req;
   let pokemonId = parseInt(params.id);
@@ -104,27 +104,32 @@ router.get("/:id", (req, res, next) => {
     let index = data.indexOf(
       data.find((pokemon) => pokemon.id === parseInt(pokemonId))
     );
-
-    result =
-      pokemonId == 1
-        ? [data[data.length - 1], data[0], data[1]]
-        : pokemonId == data[data.length - 1].id
-        ? [data[data.length - 2], data[data.length - 1], data[0]]
-        : [data[index - 1], data[index], data[index + 1]];
-    const currentPokemons = {
-      data: {
-        previousPokemon: result[0],
-        pokemon: result[1],
-        nextPokemon: result[2],
-      },
-    };
-
-    res.status(200).send(currentPokemons);
+    console.log(index);
+    if (index !== -1) {
+      result =
+        pokemonId == 1
+          ? [data[data.length - 1], data[0], data[1]]
+          : pokemonId == data[data.length - 1].id
+          ? [data[data.length - 2], data[data.length - 1], data[0]]
+          : [data[index - 1], data[index], data[index + 1]];
+      const currentPokemons = {
+        data: {
+          previousPokemon: result[0],
+          pokemon: result[1],
+          nextPokemon: result[2],
+        },
+      };
+      res.status(200).send(currentPokemons);
+    } else {
+      const exception = new Error(`No pokemon found`);
+      exception.statusCode = 401;
+      throw exception;
+    }
   } catch (error) {
     next(error);
   }
 });
-
+//Create a new pokemon
 router.post("/", (req, res, next) => {
   try {
     const { name, id, types, url } = req.body;
@@ -175,5 +180,76 @@ router.post("/", (req, res, next) => {
     next(error);
   }
 });
+//Update a pokemon
+router.put("/:pokemonId", (req, res, next) => {
+  try {
+    const allowUpdate = ["name", "id", "types", "url"];
 
+    const { pokemonId } = req.params;
+
+    const updates = req.body;
+    const updateKeys = Object.keys(updates);
+
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+
+    let db = fs.readFileSync("pokemons.json", "utf-8");
+    db = JSON.parse(db);
+    const { data } = db;
+
+    const targetIndex = data.indexOf(
+      data.find((pokemon) => pokemon.id === parseInt(pokemonId))
+    );
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+
+    const updatedPokemon = { ...db.data[targetIndex], ...updates };
+    db.data[targetIndex] = updatedPokemon;
+
+    db = JSON.stringify(db);
+
+    fs.writeFileSync("pokemons.json", db);
+
+    res.status(200).send(updatedPokemon);
+  } catch (error) {
+    next(error);
+  }
+});
+//Delete a pokemon
+router.delete("/:pokemonId", (req, res, next) => {
+  try {
+    const { pokemonId } = req.params;
+
+    let db = fs.readFileSync("pokemons.json", "utf-8");
+    db = JSON.parse(db);
+    const { data } = db;
+
+    const targetIndex = data.indexOf(
+      data.find((pokemon) => pokemon.id === parseInt(pokemonId))
+    );
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+
+    db.data = data.filter((pokemon) => pokemon.id !== parseInt(pokemonId));
+
+    db = JSON.stringify(db);
+
+    fs.writeFileSync("pokemons.json", db);
+
+    res.status(200).send({});
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
